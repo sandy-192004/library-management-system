c12dfb5979eec1a1688b32252c7f57bd525a3e56
sandy-192004sandhiyapalanivelr@gmail.com <your_email@example.com>
2025-09-04 11:37:52 +0530

    db get connected to mongodb

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-const books = require("./route/books.route.js");
+const books = require("./router/books.router.js");
+const connectDB = require("./database/db.js");
 
 app.get("/", (req,res) => {
     res.json({msg: "hello all"});
 });
 
-
+connectDB();
 app.use("/books", books);
 
 app.listen(port, (err) => {
